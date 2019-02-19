a36c9dae1b6c9acd61b14d3ca7d04a366f731fde
Sorokin <s.e.d91@mail.ru>
2019-02-19 21:40:47 +0400

    решение module-1 task-1

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -4,6 +4,22 @@
  * @param {number} n index
  * @returns {number}
  */
+"use strict";
+
+   let m = +prompt("Введите число"),
+       n = +prompt("Введите степень");
+
 function pow (m, n) {
 
-}
+   if ((m >= 1) & (n >= 1)) {
+     let res = 1;
+       for (let i=0; i<n; i++) {
+         res *= m;
+       }
+       return(res);
+   } else {
+       return("числа должны быть больше 1");
+   }
+ }
+
+ alert(pow(m,n));
