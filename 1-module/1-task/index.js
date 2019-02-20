62c6e2cd15071976a108f656fb08f686fd091001
Sorokin <s.e.d91@mail.ru>
2019-02-20 20:04:30 +0400

    решение module-1 task-1

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -5,5 +5,13 @@
  * @returns {number}
  */
 function pow (m, n) {
-
+    if ((m >= 1) & (n >= 1)) {
+        let res = 1;
+          for (let i=0; i<n; i++) {
+            res *= m;
+          }
+          return(res);
+      } else {
+          return("числа должны быть больше 1");
+      }
 }
