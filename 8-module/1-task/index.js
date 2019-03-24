542efc406f1ca1e5651e5a18415905a0cf42043a
Сорокин Дмитрий Евгеньевич <dmitriy.sorokin@sms-a.ru>
2019-03-24 16:10:31 +0400

    решение 8 модуля

--- 8-module/1-task/index.js
+++ 8-module/1-task/index.js
@@ -1,32 +1,47 @@
 'use strict';
-
-/**
- * Компонент, который реализует сортируемую таблицу
- * @param {Array} items - данные, которые нужно отобразить
- *
- * Пример одного элемента, описывающего строку таблицы
- *
- *      {
- *          name: 'Ilia',
- *          age: 25,
- *          salary: '1000',
- *          city: 'Petrozavodsk'
- *      },
- *
- * @constructor
- */
 function SortableTable(items) {
-
-    /**
-     * @property {Element} - обязательно свойство, которое ссылается на элемент <table>
-     */
+    this.a = items;
     this.el = document.createElement('table');
 
-    /**
-     * Метод выполняет сортировку таблицы
-     * @param {number} column - номер колонки, по которой нужно выполнить сортировку (отсчет начинается от 0)
-     * @param {boolean} desc - признак того, что сортировка должна идти в обратном порядке
-     */
-    this.sort = function (column, desc = false) { };
+    let thead = document.createElement('thead'),
+        tbody = document.createElement('tbody'); 
+    
+    this.el.classList.add("result__table");
+    thead.classList.add("result__Thead");
+    tbody.classList.add("result__Tbody");    
+    thead.innerHTML = `<tr><td>Name</td><td>Age</td><td>Salary</td><td>Country</td></tr>`;
+    
+    this.el.appendChild(thead);
+    this.el.appendChild(tbody);
+
+
+    function render (arr) {
+
+        tbody.innerHTML = arr.filter(item => typeof item === "object")
+               .map(item =>`<tr><td>${item.name}</td><td>${item.age}</td><td>${item.salary}</td><td>${item.city}</td></tr>`).join(" ");
+    }
+
+    function compare(field, param) {
+        return function(a, b) {
+           if (param == true) {
+            return a[field] < b[field] ? 1 : -1
+           } else {
+            return a[field] > b[field] ? 1 : -1
+           }   
+        }
+    }
+
+    this.sort = function (column, desc = false) { 
+        if (column == 0 && desc == true) {
+            this.a.sort(compare('name', desc));
+           } else if (column == 0) {
+            this.a.sort(compare('name'));
+           } else if (column == 2) {
+             this.a.sort(compare('salary'));
+           }
+        render(this.a);
+    }
+
+    render(this.a);
 }
 
